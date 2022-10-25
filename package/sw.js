6a9505d3998fe903498cfaef13da8f6c45538525
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-10-25 22:48:03 +0600

    revert

--- package/sw.js
+++ package/sw.js
@@ -1,35 +1,7 @@
-const spa = _spaEnabled_
-const assets = _preCacheFiles_
 const preCache = '_preCache_'
 const runtimeCache = '_runtimeCache_'
-
-const addFetchListener = () => {
-  self.addEventListener('fetch', (evt) => {
-    if (evt.request.url.startsWith(location.origin)) {
-      evt.respondWith(
-        caches.match(evt.request).then((cacheRes) => {
-          return (
-            cacheRes ||
-            fetch(evt.request)
-              .then((fetchRes) => {
-                return caches.open(runtimeCache).then((cache) => {
-                  cache.put(evt.request.url, fetchRes.clone())
-                  return fetchRes
-                })
-              })
-              .catch(() => {
-                if (spa) {
-                  return caches
-                    .match(new Request('/'))
-                    .then((cacheRes) => cacheRes)
-                }
-              })
-          )
-        })
-      )
-    }
-  })
-}
+const assets = _preCacheFiles_
+const spa = _spaEnabled_
 
 self.addEventListener('activate', (evt) => {
   evt.waitUntil(
@@ -37,8 +9,29 @@ self.addEventListener('activate', (evt) => {
       .keys()
       .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
       .then(() => caches.open(preCache).then((cache) => cache.addAll(assets)))
-      .then(addFetchListener)
   )
 })
 
-addFetchListener()
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.url.startsWith(location.origin)) {
+    evt.respondWith(
+      caches.match(evt.request).then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request)
+            .then((fetchRes) => {
+              return caches.open(runtimeCache).then((cache) => {
+                cache.put(evt.request.url, fetchRes.clone())
+                return fetchRes
+              })
+            })
+            .catch(() => {
+              if (spa) {
+                return caches.match('/').then((cacheRes) => cacheRes)
+              }
+            })
+        )
+      })
+    )
+  }
+})
